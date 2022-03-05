e4f97c3a2e043049697955f8c491fb23b59e68cd
Achinths Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2022-03-05 13:12:47 +0530

    utils fiile created

--- clientServer/clientServerMain.js
+++ clientServer/clientServerMain.js
@@ -29,31 +29,3 @@ module.exports = {
         }
     }
 }
-
-/* function clientServer(socket, json) {
-    switch (json["type"]) {
-        case "newidentity":
-            newidentity(socket, json["identity"]);
-            break;
-        case "list":
-            // code block
-            break;
-        case "who":
-            // code block
-            break;
-        case "createroom":
-            // code block
-            break;
-        case "joinroom":
-            // code block
-            break;
-        case "message":
-            // code block
-            break;
-        case "quit":
-            // code block
-            break;
-        default:
-            console.log(`\nERROR: Message received from client is wrong.\nPlease check and try again\n` + JSON.stringify(json));
-    }
-} */
--- clientServer/newIdentity.js
+++ clientServer/newIdentity.js
@@ -1,30 +1,20 @@
+const util = require("../util/util");
+
 module.exports = {
     newidentity: function (socket, identity) {
         let regEx = new RegExp('^[a-z][a-z0-9]{2,16}$', 'i');
 
         let obj = { "type": "newidentity", "approved": "true" };
-        let buf = Buffer.from(JSON.stringify(obj));
-        console.log(JSON.stringify(buf.toJSON()));
+        let buf = util.jsonEncode(obj);
+        console.log(JSON.stringify(buf));
 
         if (regEx.test(identity)) {
-            socket.sendMessage({ "type": "newidentity", "approved": "true" });
+            socket.sendMessage(buf);
         } else {
-            socket.sendMessage({ "type": "newidentity", "approved": "false" });
+            obj = { "type": "newidentity", "approved": "false" };
+            buf = util.jsonEncode(obj);
+            socket.sendMessage(buf);
         }
     }
 
 }
-
-/* function newidentity(socket, identity) {
-    let regEx = new RegExp('^[a-z][a-z0-9]{2,16}$', 'i');
-
-    let obj = { "type": "newidentity", "approved": "true" };
-    let buf = Buffer.from(JSON.stringify(obj));
-    console.log(JSON.stringify(buf.toJSON()));
-
-    if (regEx.test(identity)) {
-        socket.sendMessage({ "type": "newidentity", "approved": "true" });
-    } else {
-        socket.sendMessage({ "type": "newidentity", "approved": "false" });
-    }
-} */
