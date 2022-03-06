29681899b6b92629ac19e62aa5eed848a02fc39c
Achinths Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2022-03-06 11:43:51 +0530

    chatRoomManager comment changed

--- chatRoomManager/chatRoomManager.js
+++ chatRoomManager/chatRoomManager.js
@@ -2,7 +2,8 @@
 
 inculdes client details.
 [
-    clientIdentity : {
+    {   
+        clientIdentity : // clientIdentity
         socket : // associate socket object of the client
     },
     ....
@@ -15,12 +16,19 @@ var serverClients = [];
 
 inculdes chat rooms details inculded in the server
 [
-    chatRoomIdentity : {
+    {   
+        chatRoomIdentity: // chatRoomIdentity
         owner : // owner's client ID
         identities : [clients in the chat room]     
     },
     ....
 ]
+
+-- IMPORTANT!! --
+
+* ALWAYS KEEP THE MAINHALL AS THE FIRST ELEMENT : serverChatRooms[0] = MainHall
+
+* serverChatRooms CANNOT BE EMPTY
 
 */
 var serverChatRooms = [];
