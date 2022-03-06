99b2d45a9acadc8b565638ac3f4b4612913f483b
Achinths Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2022-03-06 11:44:24 +0530

    newidentity single client handle fixed

--- clientServer/newIdentity.js
+++ clientServer/newIdentity.js
@@ -1,15 +1,28 @@
+const { serverClients, serverChatRooms } = require("../chatRoomManager/chatRoomManager");
 const util = require("../util/util");
 
 module.exports = {
     newidentity: function (socket, identity) {
-        let regEx = new RegExp('^[a-z][a-z0-9]{2,16}$', 'i');
-
-        let obj = { "type": "newidentity", "approved": "true" };
+        let newIdentityAck;
+        let mainHallMoveAck;
         let buf = util.jsonEncode(obj);
-        console.log(JSON.stringify(buf));
+        console.log(socket.remotePort);
 
-        if (regEx.test(identity)) {
-            socket.sendMessage(buf);
+        if (checkAvailability(identity)) {
+            // adding the client to the clients in the server list
+            serverClients.push({
+                clientIdentity: identity,
+                socket: socket
+            });
+
+            // adding the client into MainHall
+            serverChatRooms[0].identities.push(identity);
+
+            newIdentityAck = { "type": "newidentity", "approved": "true" };
+            mainHallMoveAck = { "type": "roomchange", "identity": identity, "former": "", "roomid": serverChatRooms[0].chatRoomIdentity };
+
+            socket.write(JSON.stringify(obj) + "\n", () => console.log("Completed"));
+            socket.write(JSON.stringify(obj1) + "\n");
         } else {
             obj = { "type": "newidentity", "approved": "false" };
             buf = util.jsonEncode(obj);
@@ -17,4 +30,13 @@ module.exports = {
         }
     }
 
+}
+
+function checkAvailability(identity) {
+    let regEx = new RegExp('^[a-z][a-z0-9]{2,16}$', 'i');
+    if (regEx.test(identity)) {
+        return true;
+    }
+
+    return false;
 }
