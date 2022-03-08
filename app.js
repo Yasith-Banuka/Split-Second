c7de632863a8425edb177c1abb560f9243fd3efe
Afra Hussaindeen <mhfafra@gmail.com>
2022-03-08 18:33:43 +0530

    support client & server request handling

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const { serverChatRooms } = require('./chatRoomManager/chatRoomManager');
 const { clientServer } = require('./clientServer/clientServerMain');
 const util = require('./util/util');
 
-// The port on which the server is listening.
+// The port on which the server is listening for clients.
 const port = 8080;
+// The port in which the server used for coordination
+const coordination_port = 5555;
+const other_servers_coordination_ports = [5556, 5557];
 
 const server = new Net.Server();
 
@@ -20,36 +23,41 @@ server.listen(port, function () {
     })
 });
 
-// When a client requests a connection with the server, the server creates a new
-// socket dedicated to that client.
 
+// When a client or another server requests a connection with the server, the server creates a new socket dedicated to it.
 server.on('connection', function (socket) {
     //socket = new JsonSocket(socket);
     console.log('A new connection has been established.');
 
-
-
-    // Now that a TCP connection has been established, the server can send data to
-    // the client by writing to its socket.
+    // Now that a TCP connection has been established, the server can send data to the client by writing to its socket.
     //socket.write('Hello, client.');
 
-    // The server can also receive data from the client by reading from its socket.
-
+    // The server can also receive data from the client or another server  by reading from its socket.
     socket.on('data', function (bufObj) {
         let json = util.jsonDecode(bufObj);
 
-        //console.log(`Data received from client: ` + JSON.stringify(json));
-        console.log(`Data received from client: ` + JSON.stringify(json) + `\n`);
-        clientServer(socket, json);
+        // Check whether the established connection is from a server or a client and redirect accordingly 
+        // console.log(socket.remotePort);
+        if (other_servers_coordination_ports.includes(socket.remotePort)){
+            console.log(`Data received from server: ` + JSON.stringify(json) + `\n`);
+            serverServer(socket, json);
+        }else{
+            console.log(`Data received from client: ` + JSON.stringify(json) + `\n`);
+            clientServer(socket, json);
+        }
+        
     });
+         
 
     // When the client requests to end the TCP connection with the server, the server ends the connection.
     socket.on('end', function () {
         console.log('Closing connection with the client');
     });
 
+
     // Don't forget to catch error, for your own sake.
     socket.on('error', function (err) {
         console.log(`Error: ${err}`);
     });
 });
+
