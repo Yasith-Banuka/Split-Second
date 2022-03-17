a891e3bd257a47820a08e46b5a7d6ec8ece868b6
Achinths Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2022-03-18 00:13:40 +0530

    movejoin file added

---
+++ clientServer/joinRoom/moveJoin.js
@@ -0,0 +1,55 @@
+const { getLocalChatRoom, getMainHallID } = require("../../data/serverChatRooms");
+const { jsonEncode } = require("../../util/util");
+
+module.exports = {
+    moveJoin: function (socket, roomId, former, clientIdentity) {
+
+        let getChatRoom = getLocalChatRoom(roomId);
+
+        if (typeof getChatRoom != "boolean") {
+
+            localMoveJoin(socket, roomId, former, clientIdentity);
+
+            // send serverChange message to the client
+            let serverChange = {
+                "type": "serverchange",
+                "approved": "true",
+                "serverid": "s2"
+            };
+
+            socket.write(jsonEncode(serverChange));
+
+        } else {
+
+            // move to the MainHall of the server
+            localMoveJoin(socket, getMainHallID(), former, clientIdentity);
+
+        }
+
+
+    }
+}
+
+// common parts of the moveJoin
+function localMoveJoin(socket, roomId, former, clientIdentity) {
+    let clientObject = {
+        clientIdentity: clientIdentity,
+        socket: socket,
+        chatRoom: roomId
+    };
+
+    let approveMessage = {
+        "type": "roomchange",
+        "identity": clientIdentity,
+        "former": former,
+        "roomid": roomId
+    };
+
+    // adding the client to the server client list
+    addLocalClient(clientObject);
+
+    // adding the client into room
+    joinClientNewChatRoom(roomId, clientObject);
+
+    util.broadcast(getLocalChatRoom(roomId).clients, approveMessage);
+}
