d01f4cd4e32968e7910d131c01b2b68d7f1d9b00
Yasith-Banuka <Yasith-Banuka@users.noreply.github.com>
2022-03-20 21:38:01 +0530

    room creation fix

--- serverManager/coordinatorCommunication.js
+++ serverManager/coordinatorCommunication.js
@@ -25,7 +25,7 @@ async function getCoordinatorRoomIdApproval(roomId, serverId) {
         beginElection();
         return false;
     }
-    if(response.type === "roomconfirm" && json.roomid === roomId) {
+    if(response.type === "roomconfirm" && response.roomid === roomId) {
         return response.roomapproved;
     }
     return false;
