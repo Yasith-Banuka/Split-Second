0d776733daf2af7992e768f7c1c39746aa85fdd2
Achinths Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2022-03-23 12:45:29 +0530

    bug fix in broadcast communication

--- serverManager/broadcastCommunication.js
+++ serverManager/broadcastCommunication.js
@@ -1,54 +1,54 @@
-const {multicast} = require('./serverMessage');
-const {getCoordinator} = require('../data/serverDetails');
-const {getAllServerInfo} = require("../data/globalServerDetails");
-const {addClient, removeClient} = require('../data/globalClients');
-const {addChatroom, removeChatroom} = require('../data/globalChatRooms');
+const { multicast } = require('./serverMessage');
+const { getCoordinator } = require('../data/serverDetails');
+const { getAllServerInfo } = require("../data/globalServerDetails");
+const { addClient, removeClient } = require('../data/globalClients');
+const { addChatroom, removeChatroom } = require('../data/globalChatRooms');
 
 
 
-function broadcastNewClient (serverId, clientId){
-    let message = {type : "newclient", clientid : clientId, serverid:serverId };
+function broadcastNewClient(serverId, clientId) {
+    let message = { type: "newclient", clientid: clientId, serverid: serverId };
 
     let multicastServerIds = getServerIdsExcludingLeader();
     multicast(multicastServerIds, message);
 }
 
-function uponReceiveNewClient (serverId, clientId) {
+function uponReceiveNewClient(serverId, clientId) {
     addClient(serverId, clientId);
 }
 
-function broadcastClientDeletion (clientId) {
-    let message = {type : "endclient", clientid : clientId};
+function broadcastClientDeletion(clientId) {
+    let message = { type: "endclient", clientid: clientId };
     let multicastServerIds = getServerIdsExcludingLeader();
     multicast(multicastServerIds, message);
 }
 
-function uponReceiveClientDeletion (clientId) {
+function uponReceiveClientDeletion(clientId) {
     removeClient(clientId)
 }
 
-function broadcastNewChatroom (serverId, roomId){
-    let message = {type : "newroom", roomid : roomId, serverid: serverId};
+function broadcastNewChatroom(serverId, roomId) {
+    let message = { type: "newroom", roomid: roomId, serverid: serverId };
     let multicastServerIds = getServerIdsExcludingLeader();
     multicast(multicastServerIds, message);
 }
 
-function uponReceiveNewChatroom (serverId, roomId) {
+function uponReceiveNewChatroom(serverId, roomId) {
     addChatroom(serverId, roomId)
 }
 
-function broadcastChatroomDeletion (roomId) {
-    let message = {type : "endroom", roomid : roomId};
+function broadcastChatroomDeletion(roomId) {
+    let message = { type: "endroom", roomid: roomId };
     let multicastServerIds = getServerIdsExcludingLeader();
     multicast(multicastServerIds, message);
 }
 
-function uponReceiveChatroomDeletion (roomId) {
+function uponReceiveChatroomDeletion(roomId) {
     removeChatroom(roomId);
 }
 
-function getServerIdsExcludingLeader () {
-    let coordinatorId = getCoordinator;
+function getServerIdsExcludingLeader() {
+    let coordinatorId = getCoordinator();
     let allServersInfo = getAllServerInfo();
 
     let serverIdsExcludingLeader = [];
